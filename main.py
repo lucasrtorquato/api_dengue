b1def2bdab8edef7d434f7866f7fb1e017416472
lucasrtorquato <lucas.torquato.senai@gmail.com>
2026-06-13 14:19:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from database import Base, engine
 from routes import router
 
@@ -8,6 +8,14 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(
     title="API Combate à Dengue",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
