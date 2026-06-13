2bc3416a9f6eb86c7a4d0f8124a077af2bc6197e
lucasrtorquato <lucas.torquato.senai@gmail.com>
2026-06-13 10:19:42 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime
 from datetime import datetime
-from .database import Base
+from database import Base
 
 class FocoDengue(Base):
     __tablename__ = "focos"
